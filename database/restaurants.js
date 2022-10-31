b8f85e6a700e9b0241a30ce861911ddcae490864
Christina Moore <sayhitochristina@gmail.com>
2022-10-31 17:07:00 -0500

    delete, post, compare working

--- database/restaurants.js
+++ database/restaurants.js
@@ -5,7 +5,7 @@ const config = require('./config');
 async function getAllRestaurants(){
   const offset = helper.getOffset(config.listPerPage);
   const rows = await db.query(
-    `SELECT GROUP_CONCAT(res_name) as Restaurants FROM Restaurants`
+    `SELECT GROUP_CONCAT(res_name ORDER BY res_name DESC) as Restaurants FROM Restaurants`
   );
   const data = helper.emptyOrRows(rows);
 
@@ -13,6 +13,13 @@ async function getAllRestaurants(){
     data
   }
 }
+
+async function compare(restaurant){
+    const rows = await db.query(
+      `SELECT res_name FROM Restaurants WHERE res_name=("${restaurant.name}")`
+    );
+    return rows
+  }
 
 async function create(restaurant){
     const result = await db.query(
@@ -28,7 +35,23 @@ async function create(restaurant){
     return {message};
   }
 
+  async function remove(restaurant){
+    const result = await db.query(
+      `DELETE FROM Restaurants WHERE res_name='${restaurant.name}'`
+    );
+  
+    let message = 'Error';
+  
+    if (result.affectedRows) {
+      message = 'Deleted successfully';
+    }
+  
+    return {message};
+  }
+
 module.exports = {
     getAllRestaurants,
-    create
+    create,
+    remove,
+    compare
 }
--- routes/router.js
+++ routes/router.js
@@ -2,10 +2,18 @@ const express = require('express');
 const router = express.Router();
 const restaurants = require('../database/restaurants');
 
-/* GET programming languages. */
 router.get('/', async function(req, res, next) {
   try {
     res.json(await restaurants.getAllRestaurants(req.query.page));
+  } catch (err) {
+    console.error(`Error: `, err.message);
+    next(err);
+  }
+});
+
+router.post('/compare', async function(req, res, next) {
+  try {
+    res.json(await restaurants.compare(req.body));
   } catch (err) {
     console.error(`Error: `, err.message);
     next(err);
@@ -20,5 +28,14 @@ router.post('/add', async function(req, res, next) {
       next(err);
     }
   });
+
+router.delete('/remove', async function(req, res, next) {
+  try {
+    res.json(await restaurants.remove(req.body));
+  } catch (err) {
+    console.error(`Error: `, err.message);
+    next(err);
+  }
+});
 
 module.exports = router;
